b9e7278e416e2a066650706e4b7ee734acf9b76a
razmiO <thomas.cuesta31@gmail.com>
2025-03-24 12:45:56 +0100

    refactor : extract server handler run() out

--- index.lua
+++ index.lua
@@ -1,12 +1,17 @@
 local Raz = require("lib/server")
 
+local run = function(req, res)
+    local method = req:getMethod()
+    local path = req:getPath()
+    local protocol = req:getProtocol()
 
 
-local title = "Hello, world!"
+    res:setBody(
+        "{\"message\": \"Hello, World  ti!\"}"
+    ):send()
+end
 
-Raz:start(function(req, res)
-    res:setBody("<h1>" .. title .. "</h1>"):send()
-end, {
+Raz:start(run, {
     host = "localhost",
     port = 8080,
 })
