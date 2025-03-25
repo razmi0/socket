833320b5cf62d909be2858661a3f197e8a301f3c
razmiO <thomas.cuesta31@gmail.com>
2025-03-25 01:45:34 +0100

    refactor: simplify Request properties and methods for better readability

--- lib/request.lua
+++ lib/request.lua
@@ -1,61 +1,40 @@
 local Request = {
     -- Private properties
-    __client = nil,
-    __headers = {},
-    __method = nil,
-    __path = nil,
-    __protocol = nil,
-    __body = "",
-    __hasBody = false,
-    __bodyType = nil,
+    _headers = {},
+    method = nil,
+    path = nil,
+    protocol = nil,
+    body = "",
+    hasBody = false,
+    bodyType = nil,
 
-    -- Public methods
-    setClient = function(self, client)
-        self.__client = client
-        return self
-    end,
-
-    getHeaders = function(self)
-        return self.__headers
-    end,
-
-    getHeader = function(self, key)
-        return self.__headers[key]
-    end,
-
-    getMethod = function(self)
-        return self.__method
-    end,
-
-    getPath = function(self)
-        return self.__path
-    end,
-
-    getProtocol = function(self)
-        return self.__protocol
-    end,
-
-    getBody = function(self)
-        return self.__body
-    end,
-
-    hasBody = function(self)
-        return self.__hasBody
-    end,
-
-    getBodyType = function(self)
-        return self.__bodyType
+    header = function(self, key)
+        if not key then
+            return self._headers
+        end
+        return self._headers[key]
     end,
 
     parseBody = function(self, type)
-        local bodyType = type or self.__bodyType
+        local bodyType = {
+            expected = self._headers["Content-Type"],
+            asked = type,
+        }
+
+        if bodyType.expected ~= bodyType.asked then
+            print("Parsing body as " .. bodyType.asked .. " but expected " .. bodyType.expected)
+        end
     end,
 
-
     -- Protected methods
-    _build = function(self)
+    _build = function(self, client)
+        if not client then
+            error("No client found")
+            return
+        end
+
         function YieldLine()
-            local line, err = self.__client:receive()
+            local line, err = client:receive()
             if not err then
                 return line
             end
@@ -75,51 +54,29 @@ local Request = {
             return false -- Return early if we couldn't read the first line
         end
 
-        self.__method, self.__path, self.__protocol = line:match("(%S+)%s+(%S+)%s+(%S+)")
+        self.method, self.path, self.protocol = line:match("(%S+)%s+(%S+)%s+(%S+)")
 
         -- parse the headers
         while true do
             line = YieldLine()
             if not line or line == "" then break end
             local key, value = YieldHeader(line)
-            self.__headers[key] = value
+            self._headers[key] = value
         end
 
         -- store the body if it exists
-        if self.__headers["Content-Length"] then
-            self.__hasBody = true
-            local contentLength = tonumber(self.__headers["Content-Length"]) or 0
+        if self._headers["Content-Length"] then
+            self.hasBody = true
+            local contentLength = tonumber(self._headers["Content-Length"]) or 0
             if contentLength > 0 then
-                local body, err = self.__client:receive(contentLength)
+                local body, err = client:receive(contentLength)
                 if not err then
-                    self.__body = body
+                    self.body = body
                 else
-                    self.__hasBody = false
-                    self.__body = nil
+                    self.hasBody = false
+                    self.body = nil
                 end
             end
-        end
-
-        -- if self.__method == "POST" or self.__method == "PUT" or self.__method == "PATCH" then
-        --     print(self.__body)
-
-        --     for key, value in pairs(self.__headers) do
-        --         print(key, value)
-        --     end
-        -- end
-
-        -- determine the body type
-        if self.__headers["Content-Type"] then
-            -- local mimesTypes = {
-            --     ["application/json"] = "json",
-            --     ["application/xml"] = "xml",
-            --     ["text/plain"] = "text",
-            --     ["text/html"] = "html",
-            --     ["text/css"] = "css",
-            --     ["text/javascript"] = "javascript",
-            -- }
-
-            -- self.__bodyType = self.__headers["Content-Type"]
         end
     end,
 
