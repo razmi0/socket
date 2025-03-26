5566044a31adc9edd9402608aa232ff726f5cdcc
razmiO <thomas.cuesta31@gmail.com>
2025-03-26 23:21:38 +0100

    feat: enhance homepage with structured HTML and navigation

--- index.lua
+++ index.lua
@@ -9,7 +9,41 @@ local payload = {
 }
 
 App:get("/", function(c)
-    return c.html("<h1>Some HTML brè</h1>")
+    return c.html(
+        [[
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <meta name="google" content="notranslate">
+            <title>Hello world</title>
+            <link href="https://cdn.jsdelivr.net/npm/beercss@3.9.7/dist/cdn/beer.min.css" rel="stylesheet">
+            <script type="module" src="https://cdn.jsdelivr.net/npm/beercss@3.9.7/dist/cdn/beer.min.js"></script>
+            <script type="module" src="https://cdn.jsdelivr.net/npm/material-dynamic-colors@1.1.2/dist/cdn/material-dynamic-colors.min.js"></script>
+        </head>
+        <body>
+         <header class="tertiary-container">
+            <nav>
+                <button class="active tertiary">
+                    <span> links </span>
+                    <i>arrow_drop_down</i>
+                    <menu class="border no-wrap">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/query?foo=bar">Query</a></li>
+                        <li><a href="/json">JSON</a></li>
+                        <li><a href="/users/thomas/oui">No params</a></li>
+                        <li><a href="/users/nao/123">With params</a></li>
+                    </menu>
+                </button>
+            </nav>
+        </header>
+            <h1>Some HTML</h1>
+        </body>
+        </html>
+        ]]
+    )
 end)
 
 App:get("/query", function(c)
