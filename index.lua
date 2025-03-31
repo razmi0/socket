448ceb00477099608b38066cf6ae8804013e87ce
razmiO <thomas.cuesta31@gmail.com>
2025-03-31 14:56:59 +0200

    refactor: integrate new namings

--- index.lua
+++ index.lua
@@ -1,50 +1,43 @@
 local App = require("lib/server")
 
-local app = App.new()
 
-local payload = {
+local test_payload = {
     some_object = {
         foo = "bar"
     },
     some_array = { "foo", "bar", "baz" }
 }
 
-app:get("/", function(c)
-    return c:static({ path = "./index.html" })
-end)
+--:use("/*", function(c)
+--  return c:serve({ root = "./static" })
+--end)
 
-app:get("/index.js", function(c)
-    return c:static({ path = "./public/index.js" })
-end)
+App.new()
+    :get("/", function(c)
+        return c:serve({ path = "./index.html" })
+    end)
 
-app:get("/index.css", function(c)
-    return c:static({ path = "./public/index.css" })
-end)
-
-
-
-app:get("/query", function(c)
-    local query = c.req:query()
-    return c:json({ query = query })
-end)
-
-app:get("/json", function(c)
-    return c:json({ json = payload })
-end)
-
-app:get("/users/:name/:id", function(c)
-    return c:json({
-        with_params = c.req:param()
-    })
-end)
-
-app:get("/users/thomas/oui", function(c)
-    return c:json({
-        without_params = c.req:param()
-    })
-end)
-
-
-app:start({
-    verbose = true
-})
+    :get("/index.js", function(c)
+        return c:serve({ path = "./public/index.js" })
+    end)
+    :get("/index.css", function(c)
+        return c:serve({ path = "./public/index.css" })
+    end)
+    :get("/query", function(c)
+        local query = c.req:query()
+        return c:json({ query = query })
+    end)
+    :get("/json", function(c)
+        return c:json({ json = test_payload })
+    end)
+    :get("/users/:name/:id", function(c)
+        return c:json({
+            with_params = c.req:param()
+        })
+    end)
+    :get("/users/thomas/oui", function(c)
+        return c:json({
+            without_params = c.req:param()
+        })
+    end)
+    :start({ verbose = true })
