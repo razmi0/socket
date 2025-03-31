ca3040c2cfe8fa2c0e50b87b19a8533092674af7
razmiO <thomas.cuesta31@gmail.com>
2025-03-31 14:53:46 +0200

    feat: implement File class for file handling

---
+++ lib/file.lua
@@ -0,0 +1,36 @@
+local File = {}
+File.__index = File
+
+---@class File
+---@field find fun(path: string): string Find a file
+function File.new(root)
+    local instance = setmetatable({}, File)
+    instance.root = root
+    return instance
+end
+
+---@param path string The path to find
+---@return string|nil The content of the file
+function File:find(path)
+    if path:find("*") then
+        local files = io.popen("ls " .. self.root .. path)
+        local result = {}
+        for file in files:lines() do
+            table.insert(result, file)
+        end
+        return result
+    end
+
+    if self.root then
+        path = self.root .. path
+    end
+    local file = io.open(path, "r")
+    if not file then
+        return nil
+    end
+    local content = file:read("*a")
+    file:close()
+    return content
+end
+
+return File
