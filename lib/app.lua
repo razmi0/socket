55e286b77e28e81608cf9a74f5a30e5316802431
razmiO <thomas.cuesta31@gmail.com>
2025-04-01 18:56:40 +0200

    feat: add method to log routes in readable format with handler details

--- lib/app.lua
+++ lib/app.lua
@@ -27,10 +27,63 @@ function App:_setLogger(log)
     self._log = log
 end
 
+-- See the routes in json format
+function App:see_routes()
+    if not self._log then
+        return
+    end
+
+    local output = {}
+    local methods = { "GET", "POST", "PUT", "DELETE" }
+    local divider = "================"
+
+    local separator = function(method)
+        return "\27[34m" .. divider .. method .. divider .. "\27[0m"
+    end
+
+    for method, routeData in pairs(self._routes) do
+        -- Only process if the method is one of our specified methods.
+        if table.concat(methods):find(method) then
+            table.insert(output, separator(method))
+            local indexed = routeData.indexed
+
+            -- First pass: determine the maximum length of the path strings.
+            local maxLen = 0
+            for path, _ in pairs(indexed) do
+                if #path > maxLen then
+                    maxLen = #path
+                end
+            end
+
+            -- Second pass: construct each line with padding.
+            for path, handlers in pairs(indexed) do
+                local functions = {}
+                for i, _ in ipairs(handlers) do
+                    if i == #handlers then
+                        table.insert(functions, "handler")
+                    else
+                        table.insert(functions, "middleware" .. i)
+                    end
+                end
+
+                -- Using string.format to pad the path to maxLen characters.
+                local line = string.format("    %-"
+                    .. maxLen .. "s : [ %s ]",
+                    path, table.concat(functions, ", "))
+                table.insert(output, line)
+            end
+        end
+        table.insert(output, separator(method))
+    end
+
+
+    self._log:push(table.concat(output, "\n"), { escape = true })
+end
+
 function App:get(path, ...)
     local handlers = { ... }
     if self._log then
-        self._log:push("Registering GET " .. path)
+        self._log:push("Registering GET " .. path .. " with " .. #handlers .. " handlers")
     end
     self._routes:_add_route("GET", path, handlers)
     return self
@@ -39,7 +92,7 @@ end
 function App:post(path, ...)
     local handlers = { ... }
     if self._log then
-        self._log:push("Registering POST " .. path)
+        self._log:push("Registering POST " .. path .. " with " .. #handlers .. " handlers")
     end
     self._routes:_add_route("POST", path, handlers)
     return self
@@ -101,3 +154,16 @@ function App:_run(client)
 end
 
 return App
+
+
+-- {
+--     indexed = {
+--       ["/"] = { "<function 1>" },
+--       ["/chain"] = { "<function 2>", "<function 3>", "<function 4>" },
+--       ["/index.css"] = { "<function 5>" },
+--       ["/index.js"] = { "<function 6>" },
+--       ["/json"] = { "<function 7>" },
+--       ["/query"] = { "<function 8>" },
+--       ["/users/thomas/oui"] = { "<function 9>"
+--     }
+-- }
