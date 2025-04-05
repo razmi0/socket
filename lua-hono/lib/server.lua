77c16fb65625af71523e1ebcdd825e807234d350
razmiO <thomas.cuesta31@gmail.com>
2025-04-05 03:02:00 +0200

    refactor: simplify and enhance encapsulation

--- lua-hono/lib/server.lua
+++ lua-hono/lib/server.lua
@@ -3,12 +3,66 @@
 local socket = require("socket")
 
 
+local function close(client, logger)
+    if client then
+        if logger then
+            logger:push("Closing client")
+        end
+        client:close()
+    else
+        if logger then
+            logger:push("Client is nil")
+        end
+    end
+end
+
+
+local function main(server, app)
+    -- Starting loop
+
+    local logger = app._log
+
+    while true do
+        --
+        -- Blocking I/O: The program waits until the operation completes (e.g., waiting for a network response).
+        local client, err = server:accept()
+
+        --Timeouts: Prevents the program from hanging indefinitely by limiting how long an I/O operation can wait.
+        client:settimeout(5)
+
+        -- Handle accept errors
+        if not client then
+            if logger then
+                logger:push("server:accept() error:", err, true)
+            end
+            goto continue
+        end
+
+        if logger then
+            logger:push("Accepted client")
+        end
+
+        -- Run the app
+        app:_run(client)
+
+        -- Close the client
+        close(client, logger)
+
+        if logger then
+            logger:print()
+        end
+
+        ::continue::
+    end
+end
+
+
 ---@class Server
 ---@field _host string The host address to bind the server to
 ---@field _port number The port number to bind the server to
 ---@field start fun(self: Server, base: table): nil Start the HTTP server and begin listening for connections
+---@field new fun(app : App):Server  The server instance
 ---@field _app App The application instance
----@field _log Inspect The logger instance middleware
 
 local Server = {}
 Server.__index = Server
@@ -19,7 +73,7 @@ function Server.new(app)
     if app._log then
         instance._log = app._log
     end
-    instance._host = arg[1] or "localhost"
+    instance._host = arg[1] or "127.0.0.1"
     instance._port = tonumber(arg[2]) or 8080
     instance._app = app
     return instance
@@ -47,53 +101,41 @@ function Server:start(server_config)
         print("\27[37mLuaSocket server listening : \27[0m" .. ip .. ":" .. port)
     end
 
-    self:_loop(server)
-end
 
-function Server:_loop(server)
-    while true do
-        --
-        -- Blocking I/O: The program waits until the operation completes (e.g., waiting for a network response).
-        local client, err = server:accept()
+    -- Starting loop
 
-        --Timeouts: Prevents the program from hanging indefinitely by limiting how long an I/O operation can wait.
-        client:settimeout(5)
+    main(server, self._app)
 
-        -- Handle accept errors
-        if not client then
-            self:log("server:accept() error:", err, true)
-            goto continue
-        end
+    -- while true do
+    --     --
+    --     -- Blocking I/O: The program waits until the operation completes (e.g., waiting for a network response).
+    --     local client, err = server:accept()
 
-        if self._log then
-            self._log:push("Accepted client")
-        end
+    --     --Timeouts: Prevents the program from hanging indefinitely by limiting how long an I/O operation can wait.
+    --     client:settimeout(5)
 
-        -- Run the app
-        self._app:_run(client)
+    --     -- Handle accept errors
+    --     if not client then
+    --         self:log("server:accept() error:", err, true)
+    --         goto continue
+    --     end
 
-        -- Close the client
-        self:_close(client)
+    --     if self._log then
+    --         self._log:push("Accepted client")
+    --     end
 
-        if self._log then
-            self._log:print()
-        end
+    --     -- Run the app
+    --     self._app:_run(client)
 
-        ::continue::
-    end
-end
+    --     -- Close the client
+    --     self:_close(client)
 
-function Server:_close(client)
-    if client then
-        if self._log then
-            self._log:push("Closing client")
-        end
-        client:close()
-    else
-        if self._log then
-            self._log:push("Client is nil")
-        end
-    end
+    --     if self._log then
+    --         self._log:print()
+    --     end
+
+    --     ::continue::
+    -- end
 end
 
 return Server
